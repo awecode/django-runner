2b4db3b646fd1e7ae0d81fe3d325ec9aa922e89f
Dipesh Acharya <xtranophilist@gmail.com>
2016-03-13 18:35:38 +0545

    Exit in context menu

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ import sys
 from PyQt5.QtCore import QCoreApplication, QSettings, Qt, QObject, pyqtSignal, QSize
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow, QAction, QHBoxLayout, \
-    QVBoxLayout, QLCDNumber, QSlider, QFileDialog, QSystemTrayIcon
+    QVBoxLayout, QLCDNumber, QSlider, QFileDialog, QSystemTrayIcon, QMenu
 
 
-class Tray(object):
+class Tray(QSystemTrayIcon):
     def __init__(self, base):
-        tray_icon = QSystemTrayIcon(base.app_icon, app)
-        tray_icon.show()
+        super(Tray, self).__init__(base.app_icon, app)
+        self.base = base
+        self.menu = self.create_menu()
+
+    def create_menu(self):
+        menu = QMenu(self.base.cockpit)
+        exit_action = menu.addAction("E&xit")
+        exit_action.triggered.connect(self.base.quit)
+        self.setContextMenu(menu)
+        self.show()
 
 
 class Communicate(QObject):
@@ -26,6 +34,9 @@ class DRBase(object):
         self.status_text = 'Loading ...'
         self.cockpit = Cockpit(self)
         self.tray = Tray(self)
+
+    def quit(self):
+        return QCoreApplication.instance().quit()
 
     def set_icon(self):
         app_icon = QIcon()
@@ -48,9 +59,6 @@ class Cockpit(QMainWindow):
         self.status_bar = self.create_status_bar()
         # self.toolbar = self.create_toolbar()
         self.menu_bar = self.create_menu_bar()
-        self.setWindowTitle(self.base.settings.value('title'))
-        self.setWindowIcon(self.base.app_icon)
-        self.resize(300, 300)
         self.show_window()
 
     def create_toolbar(self):
@@ -70,7 +78,7 @@ class Cockpit(QMainWindow):
         exit_action = QAction(QIcon.fromTheme('exit'), 'E&xit', self)
         exit_action.setShortcut('Ctrl+Q')
         exit_action.setStatusTip('Exit application')
-        exit_action.triggered.connect(self.quit())
+        exit_action.triggered.connect(self.quit)
 
         restore_action = QAction(QIcon.fromTheme('system-software-update'), '&Restore', self)
         restore_action.setShortcut('Ctrl+R')
@@ -84,11 +92,16 @@ class Cockpit(QMainWindow):
         return bar
 
     def show_window(self):
+        self.setWindowTitle(self.base.settings.value('title'))
+        self.setWindowIcon(self.base.app_icon)
+        self.resize(300, 300)
         self.center()
         self.show()
 
     def quit(self):
-        return QCoreApplication.instance().quit
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            return self.base.quit()
 
     def center(self):
         qr = self.frameGeometry()
