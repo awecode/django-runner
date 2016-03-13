79357721ca5c476f9dbf4e6016c41130dd5e2df2
Dipesh Acharya <xtranophilist@gmail.com>
2016-03-13 14:13:56 +0545

    Basic GUI components

--- main.py
+++ main.py
@@ -3,15 +3,54 @@
 
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow, QAction, qApp
+
+
+class Tray():
+    pass
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.statusbar = self.statusBar()
+        self.createMenuBar()
+        self.setWindowTitle('Icon')
+
+    def createMenuBar(self):
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.quit())
+        self.menubar = self.menuBar()
+        fileMenu = self.menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+    def showWindow(self):
+        self.center()
+        self.show()
+
+    def quit(self):
+        return QCoreApplication.instance().quit
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    main_window = MainWindow()
+    main_window.showWindow()
     sys.exit(app.exec_())
